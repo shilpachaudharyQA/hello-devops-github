b75fea3292bc566a91e8c495d4f7b8df5aeb89f7
Shilpa Chaudhary <shilpachaudharyevon@gmail.com>
2026-06-14 01:50:17 +0530

    featur-branch

--- app.py
+++ app.py
@@ -5,7 +5,8 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     print("Hello DevOps - updated version")
-    print("Hello DevOps - Second version")
+    print("Hello DevOps - Second ve")
+    print("Hello DevOps - feature-branch")
     return "Hello DevOps!"
 
 if __name__ == '__main__':
